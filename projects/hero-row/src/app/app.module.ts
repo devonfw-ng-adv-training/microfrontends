cf29f4a04bfde0fee4b202efd9d91a5b767560fb
BerndOlleckCap <bernd.olleck@capgemini.com>
2019-11-07 15:55:11 +0100

    move hero row to new application

--- projects/hero-row/src/app/app.module.ts
+++ projects/hero-row/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {HeroRowComponent} from './hero-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeroRowComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
